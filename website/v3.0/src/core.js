1ff4273e1e67d24776103d2e9608baf7c58e2755
VisualSJ <itharbors@outlook.com>
2014-09-15 13:58:12 +0800

    剥离sprite-batch-node模块

--- website/v3.0/src/core.js
+++ website/v3.0/src/core.js
@@ -10625,3 +10625,165 @@ cc.LabelTTF.__getFontHeightByDiv = function (fontName, fontSize) {
     return clientHeight;
 };
 cc.LabelTTF.__fontHeightCache = {};
+var cc = cc || {};
+cc._tmp = cc._tmp || {};
+cc.associateWithNative = function (jsObj, superclass) {
+};
+cc.KEY = {
+    backspace:8,
+    tab:9,
+    enter:13,
+    shift:16,
+    ctrl:17,
+    alt:18,
+    pause:19,
+    capslock:20,
+    escape:27,
+    pageup:33,
+    pagedown:34,
+    end:35,
+    home:36,
+    left:37,
+    up:38,
+    right:39,
+    down:40,
+    insert:45,
+    Delete:46,
+    0:48,
+    1:49,
+    2:50,
+    3:51,
+    4:52,
+    5:53,
+    6:54,
+    7:55,
+    8:56,
+    9:57,
+    a:65,
+    b:66,
+    c:67,
+    d:68,
+    e:69,
+    f:70,
+    g:71,
+    h:72,
+    i:73,
+    j:74,
+    k:75,
+    l:76,
+    m:77,
+    n:78,
+    o:79,
+    p:80,
+    q:81,
+    r:82,
+    s:83,
+    t:84,
+    u:85,
+    v:86,
+    w:87,
+    x:88,
+    y:89,
+    z:90,
+    num0:96,
+    num1:97,
+    num2:98,
+    num3:99,
+    num4:100,
+    num5:101,
+    num6:102,
+    num7:103,
+    num8:104,
+    num9:105,
+    '*':106,
+    '+':107,
+    '-':109,
+    'numdel':110,
+    '/':111,
+    f1:112,
+    f2:113,
+    f3:114,
+    f4:115,
+    f5:116,
+    f6:117,
+    f7:118,
+    f8:119,
+    f9:120,
+    f10:121,
+    f11:122,
+    f12:123,
+    numlock:144,
+    scrolllock:145,
+    semicolon:186,
+    ',':186,
+    equal:187,
+    '=':187,
+    ';':188,
+    comma:188,
+    dash:189,
+    '.':190,
+    period:190,
+    forwardslash:191,
+    grave:192,
+    '[':219,
+    openbracket:219,
+    ']':221,
+    closebracket:221,
+    backslash:220,
+    quote:222,
+    space:32
+};
+cc.FMT_JPG = 0;
+cc.FMT_PNG = 1;
+cc.FMT_TIFF = 2;
+cc.FMT_RAWDATA = 3;
+cc.FMT_WEBP = 4;
+cc.FMT_UNKNOWN = 5;
+cc.getImageFormatByData = function (imgData) {
+    if (imgData.length > 8 && imgData[0] == 0x89
+        && imgData[1] == 0x50
+        && imgData[2] == 0x4E
+        && imgData[3] == 0x47
+        && imgData[4] == 0x0D
+        && imgData[5] == 0x0A
+        && imgData[6] == 0x1A
+        && imgData[7] == 0x0A) {
+        return cc.FMT_PNG;
+    }
+    if (imgData.length > 2 && ((imgData[0] == 0x49 && imgData[1] == 0x49)
+        || (imgData[0] == 0x4d && imgData[1] == 0x4d)
+        || (imgData[0] == 0xff && imgData[1] == 0xd8))) {
+        return cc.FMT_TIFF;
+    }
+	return cc.FMT_UNKNOWN;
+};
+cc.inherits = function (childCtor, parentCtor) {
+    function tempCtor() {}
+    tempCtor.prototype = parentCtor.prototype;
+    childCtor.superClass_ = parentCtor.prototype;
+    childCtor.prototype = new tempCtor();
+    childCtor.prototype.constructor = childCtor;
+};
+cc.base = function(me, opt_methodName, var_args) {
+    var caller = arguments.callee.caller;
+    if (caller.superClass_) {
+        ret = caller.superClass_.constructor.apply( me, Array.prototype.slice.call(arguments, 1));
+        return ret;
+    }
+    var args = Array.prototype.slice.call(arguments, 2);
+    var foundCaller = false;
+    for (var ctor = me.constructor; ctor; ctor = ctor.superClass_ && ctor.superClass_.constructor) {
+        if (ctor.prototype[opt_methodName] === caller) {
+            foundCaller = true;
+        } else if (foundCaller) {
+            return ctor.prototype[opt_methodName].apply(me, args);
+        }
+    }
+    if (me[opt_methodName] === caller) {
+        return me.constructor.prototype[opt_methodName].apply(me, args);
+    } else {
+        throw Error(
+            'cc.base called from a method of one name ' +
+                'to a method of a different name');
+    }
+};
